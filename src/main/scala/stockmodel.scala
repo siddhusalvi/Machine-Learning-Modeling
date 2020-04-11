80dadef6ef5f218e96765585e3b30c06680c9801
siddhusalvi <siddheshsalvi525@gmail.com>
2020-04-11 12:35:51 +0530

    ADDED : python script connectivity

--- src/main/scala/stockmodel.scala
+++ src/main/scala/stockmodel.scala
@@ -1,10 +1,12 @@
 import java.io.FileWriter
+
 import org.apache.spark.sql.SparkSession
+
 import scala.collection.mutable.ListBuffer
 
 object stockmodel {
-  def main(args: Array[String]) = {
-    val spark = SparkSession.builder().appName("s3_file").master("local").getOrCreate()
+  def main(args: Array[String]): Unit = {
+    val spark = SparkSession.builder().appName("Stock_prediction").master("local").getOrCreate()
 
     val bucket = "marketdatas"
     val file = "stockdata.csv"
@@ -16,12 +18,24 @@ object stockmodel {
     spark.sparkContext.hadoopConfiguration.set("fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem")
 
     val text = spark.read.textFile("s3a://" + bucket + "/" + file)
+
     var data = ListBuffer[String]()
     for (line: String <- text.collect()) {
       data += line
     }
-    val writer = new FileWriter("C:\\Users\\Siddesh\\Desktop\\" + file, true)
+
+    val output_path = "C:\\Users\\Siddesh\\IdeaProjects\\stockdata\\src\\resources\\" + file
+    val writer = new FileWriter(output_path, true)
+    //Saving csv file on local storage
     data.foreach(c => writer.write(c.toString + "\n"))
     writer.close()
+
+    val script = "python C:\\Users\\Siddesh\\IdeaProjects\\stockdata\\src\\resources\\stockprice.py"
+    val pkl_file = "C:\\Users\\Siddesh\\IdeaProjects\\stockdata\\src\\resources\\output.pkl"
+    val command = script + " " + output_path + " " + pkl_file
+    //creating dummy RDD to pipe python script
+    val dummy_data = spark.sparkContext.parallelize(List("a", "b"))
+    val operation = dummy_data.pipe(command)
+    operation.collect()
   }
 }
